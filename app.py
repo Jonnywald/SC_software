bfc8e369512e994eafc306054225e4910b10fff9
jonnywald <delrio.guilherme@gmail.com>
2023-08-06 19:53:53 -0400

    First "working" version

--- app.py
+++ app.py
@@ -3,13 +3,8 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
-
-@app.route('/navbar')
-def navbar():
-    options = ['Options', 'Clientes', 'Vendas', 'Recebimentos']
-    return render_template('navbar.html', options=options)
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
